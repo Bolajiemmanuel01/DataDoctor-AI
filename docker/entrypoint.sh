e9ea71bf29eaf138c242c059ea31f57eefd9f8da
USER <bolajiemmanuel01@gmail.com>
2026-06-15 21:52:39 +0100

    Update Docker configuration to run Django server directly in docker-compose

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,6 +8,8 @@ done
 
 echo "PostgreSQL started"
 
-python manage.py migrate
+exec "$@"
 
-python manage.py runserver 0.0.0.0:8000
+# python manage.py migrate
+
+# python manage.py runserver 0.0.0.0:8000
